997ef6ea51077a71a10e8c9257a366097d0d52bf
ValovVK <vadimka_1995666@bk.ru>
2021-12-22 13:20:38 +1100

    finish task #4

---
+++ Task #4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число A - ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B - ");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число C - ");
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+if (max < numberB)
+{
+    max = numberB;
+}
+if (max < numberC)
+{
+    max = numberC;
+}
+Console.Write(max);
