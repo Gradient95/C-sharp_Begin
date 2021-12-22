4c08f21398081503aa4cd9699274d33ec87fb3e7
ValovVK <vadimka_1995666@bk.ru>
2021-12-22 13:18:54 +1100

    finish task #1

---
+++ Task #1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число А => ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B => ");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Является ли число A квадратом числа B?");
+if (numberA == numberB * numberB)
+{
+    Console.Write("Верно!");
+}
+else
+{
+    Console.Write("Неверно!");
+}
