b7556e54194f27b7bdeb938a02ee24c32429b5e2
ValovVK <vadimka_1995666@bk.ru>
2021-12-22 13:21:43 +1100

    finish task #7

---
+++ Task #7/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число нижнего диапазона - ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число верхнего диапазона - ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.Write("Неверный ввод");
+}
+else while (numberA <= numberB)
+    {
+        Console.Write("  " + numberA);
+        numberA = numberA + 1;
+    }
