61f92752b5269629795cd56e58f7b27ecf597a3e
ValovVK <vadimka_1995666@bk.ru>
2021-12-22 13:20:13 +1100

    finish task #3.1

---
+++ Task #3.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] array = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+Console.Write("Введите номер дня недели - ");
+int number = int.Parse(Console.ReadLine());
+if (number > 7)
+{
+    Console.Write("Неправильно введен номер");
+}
+else
+    Console.Write(array[number - 1]);
