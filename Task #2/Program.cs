cf19da939840f25186589b8461d3d4c78a33b40f
ValovVK <vadimka_1995666@bk.ru>
2021-12-22 13:19:29 +1100

    finish task #2

---
+++ Task #2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число A - ");
+double numberA = double.Parse (Console.ReadLine());
+Console.Write("Введите число B - ");
+double numberB = double.Parse (Console.ReadLine());
+if  (numberA > numberB)
+{
+    Console.Write("numberA>numberB");
+}
+else  if (numberA == numberB)
+{
+    Console.Write("numberA=numberB");
+}
+else 
+{
+    Console.Write("numberB>numberA");
+}
