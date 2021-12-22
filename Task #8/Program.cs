cf3956cadbecfc7759685c6bc743d2214072318c
ValovVK <vadimka_1995666@bk.ru>
2021-12-22 13:21:58 +1100

    finish task #8

---
+++ Task #8/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите последнее число диапазона => ");
+int X = int.Parse(Console.ReadLine());
+int i = 0;
+while (i < X)
+{
+    i++;
+    if (i % 2 == 0)
+    {
+        Console.Write("{0} ", i);
+    }
+}
